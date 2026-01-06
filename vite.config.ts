07f6fde141be55a20d024035b0cecce832645176
a5-837 <ahraz.imran@atom5engineering.com>
2026-01-06 18:32:14 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  
+  base: mode === 'production' ? '/cms_ui/' : '/',
+  
   server: {
     host: "::",
     port: 8080,
